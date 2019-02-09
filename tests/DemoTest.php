c92acf16686502169123fbd6aa6ab7dd00e30cca
Roger Cendrós <rogercbe@gmail.com>
2019-02-09 17:39:15 +0100

    First commit

---
+++ tests/DemoTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Kata\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DemoTest extends TestCase
+{
+    /** @test */
+    function it_works()
+    {
+        $this->assertTrue(true);
+    }
+}
